f7b2b3b5f5d10315ef7f0a3e519d0b7b8b7b1fae
lazy_boy99 <brechka.a.wm@gmail.com>
2020-08-14 13:37:35 +0700

    modify delete functions

--- TextFormatter/TxtFormatter.cs
+++ TextFormatter/TxtFormatter.cs
@@ -2,34 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TextFormatter
 {
     public class TxtFormatter : FileFormatter
     {
 
-
-        public TxtFormatter(int Length, string delim) : base(Length, delim) { }
+        private readonly Regex Pattern;
+        public TxtFormatter(int Length, string delim) : base(Length,delim) {
+            Pattern = new Regex("["+delim+"]");
+        }
         public TxtFormatter(int Length) : base(Length) { }
 
         public override string DeleteSymbols(string line)
         {
-            return line.Trim(Delims);
+            return Pattern.Replace(line, " ");
         }
 
         public override string DeleteWords(string line)
         {
-            var words =line.Split(' ');
+            var set = new HashSet<char>(Delims.ToString()+' ');
             var sb = new StringBuilder();
-            foreach (var word in words) {
-                if (word.Length < WordLength) { 
-                    sb.Append(word);
-                    sb.Append(" ");
+            var result= new StringBuilder();
+            for (int i=0;i<line.Length;i++) {
+                if (set.Contains(line[i])) {
+                    if (sb.Length < WordLength) {
+                        result.Append(sb);
+                        result.Append(line[i]);
+                    }
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append(line[i]);
                 }
             }
-            if(words.Length>0)
-                sb.Remove(sb.Length - 1, 1);
-            return sb.ToString();
+            return result.ToString();
         }
     }
 }
